8862648bf208070f8050361f07f311cfa70bbbcb
ThoseGrapefruits <lmooore@gmail.com>
2014-05-13 00:19:55 -0700

    Moved / resized a few objects

--- src/main/Main.java
+++ src/main/Main.java
@@ -34,27 +34,26 @@ public class Main
 		building.addPerson( new Tai( building, 400, 450 ) );
 
 		// Outer doors/walls
-		building.walls.add( new Wall( building, 750, 50, 390 + Constants.FLOOR_HEIGHT ) );
-		building.walls.add( new Wall( building, 50, 50, 390 + Constants.FLOOR_HEIGHT ) );
-		building.doors.add( new Door( building, 750, 440 + Constants.FLOOR_HEIGHT ) );
-		building.doors.add( new Door( building, 50, 440 + Constants.FLOOR_HEIGHT ) );
+		building.walls.add( new Wall( building, 750, 50, 390 ) );
+		building.walls.add( new Wall( building, 50, 50, 390 ) );
+		building.doors.add( new Door( building, 750, 440 ) );
+		building.doors.add( new Door( building, 50, 440 ) );
 
 		// Inner doors/walls
-		building.doors.add( new Door( building, 350, 440 + Constants.FLOOR_HEIGHT ) );
+		building.doors.add( new Door( building, 350, 440 ) );
 		building.walls.add( new Wall( building, 350, 440 - Constants.FLOOR_HEIGHT,
-				Constants.FLOOR_DISTANCE - Constants.DOOR_HEIGHT ) );
+				Constants.FLOOR_DISTANCE - Constants.DOOR_HEIGHT - Constants.FLOOR_HEIGHT ) );
 
 		// Floors
-		building.addFloor( 50, 550, 500 );
-		building.addFloor( 50, 550, 500 );
-		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE, 500 );
-		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 2, 500 );
-		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 3, 500 );
-		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 4, 500 );
+		building.addFloor( 50, 550, 800 );
+		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE, 700 );
+		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 2, 700 );
+		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 3, 700 );
+		building.addFloor( 50, 550 - Constants.FLOOR_DISTANCE * 4, 700 );
 
 		// Elevators
 		building.addElevator( 200.0, 50.0, new int[]
-		{ 0, 1, 2, 3, 4 } );
+		{ 0, 1, 2, 3 } );
 
 		// Create the visuals
 		SwingUtilities.invokeLater( new Runnable()
