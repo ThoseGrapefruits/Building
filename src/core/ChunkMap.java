291c47f7a7d97f0b7ec08626c7dc8dc24785b4d2
Logan Moore <lmooore@gmail.com>
2015-01-11 10:36:57 -0800

    Resource locations updated

---
+++ src/core/ChunkMap.java
@@ -0,0 +1,31 @@
+package core;
+
+import java.util.HashMap;
+
+import people.Chunk;
+
+public class ChunkMap extends HashMap < Double, Chunk >
+{
+
+	/**
+	 * Generated Serial Version UID
+	 */
+	private static final long serialVersionUID = 4753668060351034418L;
+
+	/*
+	 * public void merge( ChunkMap other )
+	 * {
+	 * for ( Map.Entry < Double, Chunk > cursor : this.entrySet() )
+	 * {
+	 * cursor.getK
+	 * }
+	 * for ( Chunk b : iter )
+	 * {
+	 * if ( !this.contains( b ) )
+	 * {
+	 * this.add( b );
+	 * }
+	 * }
+	 * }
+	 */
+}
